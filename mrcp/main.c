f30577b97f5a57ba0ed4a2afd7162ce44dc28ff8
vojtsek <vojta.hudecek@gmail.com>
2016-08-25 14:41:31 +0200

    fixes

--- mrcp/main.c
+++ mrcp/main.c
@@ -13,12 +13,13 @@
 // see http://www.unimrcp.org/dox/html/
 // and http://www.unimrcp.org/manuals/pdf/ClientConfigurationManual.pdf
 apt_bool_t msg_handler(const mrcp_app_message_t* message) {
+    // TODO: msg handling
     return (apt_bool_t) 0;
 }
 int main(int argc, const char *argv[])
 {   
     apr_status_t rv;
-    apr_pool_t *mp;
+    apr_pool_t* mp;
 
     rv = apr_initialize();
     if (rv != APR_SUCCESS) {
@@ -28,14 +29,22 @@ int main(int argc, const char *argv[])
     apr_pool_create(&mp, NULL);
 
     apt_dir_layout_t* dl = apt_default_dir_layout_create("/unimrcp-1.1.0", mp);
-    mrcp_client_t *mrcp_client = unimrcp_client_create(dl);
-    mrcp_application_t *mrcp_app = mrcp_application_create(msg_handler, NULL, mp);
+    mrcp_client_t* mrcp_client = unimrcp_client_create(dl);
+    mrcp_application_t* mrcp_app = mrcp_application_create(msg_handler, NULL, mp);
     mrcp_client_application_register(mrcp_client, mrcp_app, "TTS");
     mrcp_client_start(mrcp_client);
-    mrcp_session_t *mrcp_session = mrcp_application_session_create(mrcp_app, "uni2", NULL);
+    mrcp_session_t* mrcp_session = mrcp_application_session_create(mrcp_app, "uni2", NULL);
     mpf_termination_t termination;
-    mrcp_channel_t *mrcp_channel = mrcp_application_channel_create(mrcp_session, MRCP_SYNTHESIZER_RESOURCE, &termination, NULL, NULL);
+    mrcp_channel_t* mrcp_channel = mrcp_application_channel_create(mrcp_session, MRCP_SYNTHESIZER_RESOURCE, &termination, NULL, NULL);
     mrcp_application_channel_add(mrcp_session, mrcp_channel);
+    
+    // http://www.unimrcp.org/dox/html/mrcp__message_8h.html#adb503b7a264046b6e305930c50b02fd2
+    //mrcp_message_t* msg = mrcp_message_create(mp);
+    //mrcp_application_message_send(mrcp_session,mrcp_channel,mrcp_message);
+    //on_message_receive(mrcp_app,mrcp_session_mrcp_channel,mrcp_message);
+    
+    mrcp_application_channel_remove(mrcp_session,mrcp_channel);
+    mrcp_application_session_terminate(mrcp_session);
     apr_pool_destroy(mp);
 
     apr_terminate();
